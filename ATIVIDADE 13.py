8ef528abd0175f24dbeeaf0379eaee1b83020770
Nathalia Soares+ <n.suosso@gmail.com>
2021-05-24 16:20:10 -0300

    finalizada

---
+++ ATIVIDADE 13.py
@@ -0,0 +1,29 @@
+#Criando uma lista vazia chamada "usuarios"
+usuarios = []
+
+#Criando um usuario como dicionario com name, username e password
+usuario = {
+    "name": "Clark Kent",
+    "username" : "kent",
+    "password" : "6c70795dc50a2d6765a44bceda2699c48a8e81effe3f9e7cf3b6f8bb34ccb5b2"
+}
+# Inclui este usuario na lista de usuarios
+usuarios.append(usuario)
+
+#Criando um usuario como dicionario com name, username e password
+usuario = {
+    "name": "Bruce Wayne",
+    "username" : "wayne",
+    "password" : "0bf3116d14ceeb7b8859abb9f5b90a7747913185930fed774a949cc8777a990a"
+}
+# Inclui este usuario na lista de usuarios
+usuarios.append(usuario)
+
+#Criando um usuario como dicionario com name, username e password
+usuario = {
+    "name": "Christopher Walker",
+    "username" : "walker",
+    "password" : "db7e65b576f6b3db9041cbddff92f9a4b7253b795aab817d3f348977b014cd83"
+}
+# Inclui este usuario na lista de usuarios
+usuarios.append(usuario)
