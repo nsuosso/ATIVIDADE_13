6af18a6e1998955436ed4bb71be75e8a62663424
Nathalia Soares+ <n.suosso@gmail.com>
2021-05-23 22:47:30 -0300

    terminal

--- cryptografiakent.py
+++ cryptografiakent.py
@@ -9,7 +9,11 @@ senha2 = b"k@ne1939FInGer"
 senha3 = b"F@lK1936bEnG@1@"
 
 #hash_number = hashlib.sha256(senhan).hexadigest()
+
 hash_number1 = hashlib.sha256(senha1).hexdigest()
 print(hash_number1)
-
+hash_number2 = hashlib.sha256(senha2).hexdigest()
+print(hash_number2)
+hash_number3 = hashlib.sha256(senha3).hexdigest()
+print(hash_number3)
 
