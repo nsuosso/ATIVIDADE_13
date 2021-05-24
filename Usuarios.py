3a96fcd2a80fb58b4d352af624dda58d404e2057
Nathalia Soares+ <n.suosso@gmail.com>
2021-05-24 01:45:07 -0300

    loop

--- Usuarios.py
+++ Usuarios.py
@@ -1,9 +1,15 @@
+username0 = ["kent", "wayne", "walker"]
 username1 = "kent"
 username2 = "wayne"
 username3 = "walker"
 
-print("Insira seu Username:")
-if input:"username1", "username2", "username3"
-print("Insira sua senha:")
-if input: ""
-print("Insira seu Username:")
+input1 = "kent"
+
+# Insira seu username = username
+input("Insira seu username:")
+if username0 is "kent":
+   input("Insira sua senha:")
+if username0 is not "kent":
+    input("Insira seu username:")
+while username0 is not "kent":
+    input("Insira seu username:")
