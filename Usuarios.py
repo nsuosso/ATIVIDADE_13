3934f4c2794a37ef9e52f3004bca457ff4017722
Nathalia Soares+ <n.suosso@gmail.com>
2021-05-24 00:13:54 -0300

    Loop

--- Usuarios.py
+++ Usuarios.py
@@ -1,3 +1,9 @@
-usuarios = [kent, wayne, walker]
+username1 = "kent"
+username2 = "wayne"
+username3 = "walker"
 
-
+print("Insira seu Username:")
+if input:"username1", "username2", "username3"
+print("Insira sua senha:")
+if input: ""
+print("Insira seu Username:")
