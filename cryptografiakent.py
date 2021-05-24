ea2c46f7f8f8ea17079b16dc697937814d44e53b
Nathalia Soares+ <n.suosso@gmail.com>
2021-05-23 22:45:42 -0300

    senhas

--- cryptografiakent.py
+++ cryptografiakent.py
@@ -1,8 +1,15 @@
 import hashlib 
 h = hashlib.new('sha256')
 
-
-
+h.update(b"senhan")
 h.hexdigest()
+
+senha1 = b"Kryp10N1938"
+senha2 = b"k@ne1939FInGer"
+senha3 = b"F@lK1936bEnG@1@"
+
+#hash_number = hashlib.sha256(senhan).hexadigest()
+hash_number1 = hashlib.sha256(senha1).hexdigest()
+print(hash_number1)
 
 
